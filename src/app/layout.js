d8d714e90c1427e450055ac0e680b6f04d60600c
Shin Andrei Riego <riegoshinandrei@gmail.com>
2023-06-29 19:03:26 +0800

    Create basic main page

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header class="h-16 bg-[#1F2937]">
+          <div class="h-full container-auto px-6 flex justify-between items-center">
+            <span class="font-poppins font-semibold text-2xl text-white tracking-wide">
+              blog.
+            </span>
+          </div>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
