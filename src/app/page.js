71c23c33ccd54409c734aee7b0186ec0e2bd4f31
Shin Andrei Riego <riegoshinandrei@gmail.com>
2023-06-29 19:48:05 +0800

    Use env var for API url

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 async function fetchPosts() {
-  const res = await fetch('http://127.0.0.1:8080/posts');
+  const res = await fetch(`${process.env.API_URL}/posts`);
   const { posts } = await res.json();
 
   return posts;
